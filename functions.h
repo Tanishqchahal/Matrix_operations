5949865512c6f0e3a24d6a71fcd4ea7d9efaa187
Tanishq Chahal <70264281+Tanishqchahal@users.noreply.github.com>
2024-04-06 10:43:06 -0400

    new function headers for dynamic memory allocation

--- functions.h
+++ functions.h
@@ -2,11 +2,11 @@
 #define FUNCTIONS_H
 // DO NOT change anything here. You will not submit this file.
 // Function prototypes
-void generateMatrix(int rows, int cols, double matrix[rows][cols]);
-void printMatrix(int rows, int cols, double matrix[rows][cols]);
-void addMatrices(int N1, int M1, double A[N1][M1], int N2, int M2, double B[N2][M2], double result[N1][M1]);
-void subtractMatrices(int N1, int M1, double A[N1][M1], int N2, int M2, double B[N2][M2], double result[N1][M1]);
-void multiplyMatrices(int N1, int M1, double A[N1][M1], int N2, int M2, double B[N2][M2], double result[N1][M2]);
-void solveLinearSystem(int N1, int M1, double A[N1][M1], int N2, int M2, double B[N2][M2], double x[N1][M2]);
-
+void generateMatrix(int rows, int cols, double **matrix);
+void printMatrix(int rows, int cols, double **matrix);
+void addMatrices(int N1, int M1, double **A, int N2, int M2, double **B, double **result);
+void subtractMatrices(int N1, int M1, double **A, int N2, int M2, double **B, double **result);
+void multiplyMatrices(int N1, int M1, double **A, int N2, int M2, double **B, double **result);
+void solveLinearSystem(int N1, int M1, double **A, int N2, int M2, double **B, double **x);
+void deallocateMatrix(int rows, double **matrix);
 #endif /* FUNCTIONS_H */
